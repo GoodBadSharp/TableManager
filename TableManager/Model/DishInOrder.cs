6157579af7f917dfd81965438dde888b8963c858
Екатерина Ивашнева <evivashneva@edu.hse.ru>
2017-12-02 19:36:04 +0300

    Classes representing subject area added w/ Julia

---
+++ TableManager/Model/DishInOrder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class DishInOrder
+    {
+        public int Id { get; set; }
+        public Dish Dish { get; set; }
+        public Order Order { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ TableManager/Model/Table.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Table
+    {
+        public int Id { get; set; }
+        public enum State
+        {
+            Reserved, Occupied, Vacant
+        }
+        public List<Order> CurrentOrders { get; set; }
+    }
+}
